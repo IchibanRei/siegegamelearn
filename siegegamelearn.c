49709b2e05860914612a78c486119c01d90e1201
Rey Perez <ichibanrei@gmail.com>
2025-07-20 11:17:28 -0700

    Complete

--- siegegamelearn.c
+++ siegegamelearn.c
@@ -1,93 +1,103 @@
 
-#include <conio.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <peekpoke.h>
-#include <string.h>
-#include <c64.h>
-#include <cbm_petscii_charmap.h>
-
 #include "common.h"
-//#link "common.c"
 
+// get the character at a specfic x/y position
+byte readcharxy(byte x, byte y) {
+  return PEEK(SCRNADR(0x400, x, y));
+}
+
+// delay for 'count' frames
 void delay(byte count) {
-  while(count--) {
+  while (count--) {
     waitvsync();
   }
 }
 
-typedef struct{
-  byte x; 	  // x coordinate
-  byte y; 	  // y coordinate
-  byte dir; 	  // direction 0-3
-  byte score;	  // current score
-  char head_attr; // char to draw player
-  char tail_attr; // char to draw trail
-  bool collided;  // did this player collide
-  bool human;	  // is this player a human
+typedef struct {
+  byte x;          // x coordinate
+  byte y;          // y coordinate
+  byte dir;        // direction (0-3)
+  byte score;      // current score
+  char head_attr;  // char to draw player
+  char tail_attr;  // char to draw trail
+  bool collided;  // did we collide?
+  bool human;     // is this player a human?
 } Player;
-Player players[2]; // two players
 
-byte frames_per_move; // speed of the game
+Player players[2]; // player #0 and #1 data
+
+byte frames_per_move;  // speed of game
+byte gameover;         // = 1 if game is over
 
 #define START_SPEED 12
 #define MAX_SPEED 5
 #define MAX_SCORE 7
 
-typedef enum { D_RIGHT, D_DOWN, D_LEFT, D_UP } Direction;
 const char BOX_CHARS[8] = { '+', '+', '+', '+',
- 		 	    '-', '-', '|', '|' };
-const sbyte DIR_X[4] = { 1, 0, -1, 0 };
-const sbyte DIR_Y[4] = { 0, -1, 0, 1 };
+                            '-', '-', '!', '!'};
 
-void draw_box(byte x, byte y, byte x2, byte y2, const char* chars){
+// draw a box from coordinate (x,y) to (x2,y2)
+void draw_box(byte x, byte y, byte x2, byte y2, const char* chars) {
   byte x1 = x;
   cputcxy(x, y, chars[2]);
   cputcxy(x2, y, chars[3]);
   cputcxy(x, y2, chars[0]);
   cputcxy(x2, y2, chars[1]);
-  while (++x < x2){
+  while (++x < x2) {
     cputcxy(x, y, chars[5]);
     cputcxy(x, y2, chars[4]);
   }
-  while (++y < y2){
+  while (++y < y2) {
     cputcxy(x1, y, chars[6]);
     cputcxy(x2, y, chars[7]);
   }
 }
 
+// draw the playfield border and score
 void draw_playfield() {
-  draw_box(0, 1, COLS-1, ROWS-1, BOX_CHARS);
+  draw_box(0,1,COLS-1,ROWS-1,BOX_CHARS);
   cputsxy( 0, 0, "plyr1:");
   cputsxy(20, 0, "plyr2:");
   cputcxy( 7, 0, players[0].score+'0');
   cputcxy(27, 0, players[1].score+'0');
 }
 
-void init_game(){
+// constants for the 4 cardinal directions
+typedef enum { D_RIGHT, D_DOWN, D_LEFT, D_UP } dir_t;
+const sbyte DIR_X[4] = { 1, 0, -1, 0 };
+const sbyte DIR_Y[4] = { 0, 1, 0, -1 };
+
+// initialize game and player data
+void init_game() {
   memset(players, 0, sizeof(players));
   players[0].head_attr = '1';
-  players[0].tail_attr = '#';
   players[1].head_attr = '2';
+  players[0].tail_attr = '#';
   players[1].tail_attr = '*';
   frames_per_move = START_SPEED;
 }
 
+// reset players to initial conditions
 void reset_players() {
   players[0].x = players[0].y = 5;
   players[0].dir = D_RIGHT;
-  players[1].x = COLS - 6;
-  players[1].y = ROWS - 6;
+  players[1].x = COLS-6;
+  players[1].y = ROWS-6;
   players[1].dir = D_LEFT;
   players[0].collided = players[1].collided = 0;
 }
 
-byte readcharxy (byte x, byte y) {
-  return PEEK(SCRNADR(0x400, x, y));
+// draw player character at head
+void draw_player(Player* p) {
+  cputcxy(p->x, p->y, p->head_attr);
 }
 
-void draw_player (Player* p) {
-  cputcxy(p->x, p->y, p->head_attr);
+void move_player(Player* p) {
+  cputcxy(p->x, p->y, p->tail_attr);
+  p->x += DIR_X[p->dir];
+  p->y += DIR_Y[p->dir];
+  if((readcharxy(p->x, p->y) & 0x7f) != ' ') p->collided = 1;
+  draw_player(p);
 }
 
 void flash_colliders() {
@@ -103,23 +113,12 @@ void flash_colliders() {
   revers(0);
 }
 
-void play_round() {
-  reset_players();
-  clrscr();
-  textcolor(COLOR_WHITE);
-  draw_playfield();
-  while(1) {
-    if (players[0].collided || players[1].collided) break;
-  }
-  flash_colliders();
-}
-
 void human_control(Player *p) {
   byte dir = 0xff;
   char joy;
   if(!p->human) return; // if not a human return
   if(!kbhit()) return; // return if no key hit
-  joy = joy_read(0); // read first joystick
+  joy = joy_read(1); // read first joystick
   if(JOY_UP(joy))dir = D_UP;
   if(JOY_LEFT(joy))dir = D_LEFT;
   if(JOY_RIGHT(joy))dir = D_RIGHT;
@@ -130,13 +129,13 @@ void human_control(Player *p) {
   }
 }
 
-byte ai_try_dir(Player *p, Direction dir, byte shift) {
+byte ai_try_dir(Player *p, dir_t dir, byte shift) {
   byte x, y;
-  dir &=3;
+  dir &= 3;
   x = p->x + (DIR_X[dir] << shift);
   y = p->y + (DIR_Y[dir] << shift);
-  if(x < COLS && y < ROWS
-     && (readcharxy(x, y) & 0x7f) == ' ') {
+  if(x < COLS && y < ROWS && (readcharxy(x, y) & 0x7f) == ' ') {
+    p->dir = dir;
     return 1;
   }
   else {
@@ -144,36 +143,29 @@ byte ai_try_dir(Player *p, Direction dir, byte shift) {
   }
 }
 
-void ai_control(Player *p) {
-  Direction dir;
-  if(!ai_try_dir(p, dir, 0)) { //blocked ahead
-    ai_try_dir(p, dir+1, 0); // try turning right
-    ai_try_dir(p, dir-1, 0); // try turning left
-  }
-  else {
+// AI computer player routine
+void ai_control(Player* p) {
+  dir_t dir;
+  if (p->human) return;
+  dir = p->dir;
+  if (!ai_try_dir(p, dir, 0)) {
+    ai_try_dir(p, dir+1, 0);
+    ai_try_dir(p, dir-1, 0);
+  } else {
     ai_try_dir(p, dir-1, 0) && ai_try_dir(p, dir-1, 1+(rand() & 3));
     ai_try_dir(p, dir+1, 0) && ai_try_dir(p, dir+1, 1+(rand() & 3));
     ai_try_dir(p, dir, rand() & 3);
-  } 
-}
-
-void move_player(Player* p) {
-  cputcxy(p->x, p->y, p->tail_attr);
-  p->x += DIR_X[p->dir];
-  p->y += DIR_Y[p->dir];
-  if((readcharxy(p->x, p->y) & 0x7f) != ' ') {
-    p->collided = 1;
-    draw_player(p);
   }
 }
 
 void make_move() {
   byte i;
-  for (i = 0; i < frames_per_move; i++) {
+  for (i = 0; i<frames_per_move; i++) {
     human_control(&players[0]);
+    delay(1);
   }
-  //ai_control(&players[0]);
-  //ai_control(&players[1]);
+  ai_control(&players[0]);
+  ai_control(&players[1]);
   // if players collide, 2nd player gets the point
   textcolor(COLOR_CYAN);
   move_player(&players[1]);
@@ -182,11 +174,53 @@ void make_move() {
   textcolor(COLOR_WHITE);
 }
 
+// end of game, show the winner
+void declare_winner(byte winner) {
+  byte i;
+  clrscr();
+  for (i=0; i<ROWS/2-3; i++) {
+    draw_box(i,i,COLS-1-i,ROWS-1-i,BOX_CHARS);
+    delay(1);
+  }
+  cputsxy(12,10,"WINNER:");
+  cputsxy(12,13,"PLAYER ");
+  cputcxy(12+7, 13, '1'+winner);
+  delay(200);
+  gameover = 1;
+}
+
+void play_round() {
+  reset_players();
+  clrscr();
+  textcolor(COLOR_WHITE);
+  draw_playfield();
+  while(1) {
+    make_move();
+    if (players[0].collided || players[1].collided) break;
+  }
+  flash_colliders();
+  // add scores to players taht didn't collide
+  if (players[0].collided) players[1].score++;
+  if (players[1].collided) players[0].score++;
+  // increase speed
+  if (frames_per_move > MAX_SPEED) frames_per_move--;
+  // game over?
+  if (players[0].score >= MAX_SCORE) declare_winner(0);
+  else if (players[1].score >= MAX_SCORE) declare_winner(1);
+}
+
+
+void play_game() {
+  gameover = 0;
+  init_game();
+  players[0].human = 1;
+  while(!gameover) {
+    play_round();
+  }
+}
+
 
 void main(void) {
-  clrscr();
-  draw_playfield();
-  getchar();
-  make_move();
-  
+  joy_install (joy_static_stddrv);
+  play_game();
 }
